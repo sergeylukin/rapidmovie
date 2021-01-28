c3632a5da312e907b48bbcfb432d1550021af787
Sergey Lukin <contact@sergeylukin.com>
2021-01-29 01:07:17 +0200

    Support live-reloading during backend development
    Developer experience changes:
    
    - Admin UI moved from http://localhost:1337/admin to
      http://localhost:1338/admin and got Live Reloading (incl. static assets)
    
    - API endpoints are still served from http://localhost:1337
    
    - Now backend code can be modified and Live reload is in place including
      it's frontend assets (powered by webpack)
    
    - From now on only `npm` is used across the project, yarn.lock was removed
      in favor of package-lock.json. Both `yarn` and `npm work, the only reason
      for this change - convention.
    
    Lower level improvements:
    
    - fix symlink for `node_modules/.bin` folder - no need to install binaries
      globally, it's enough to `npm install <package>` and executed it's binary
      from `./node_modules/.bin/<package>`
    
    - fix symlink for `node_modules/.cache` folder - optimize npm
    
    - Replace [strapi/strapi](https://hub.docker.com/r/strapi/strapi) image
      with our custom developer-friendly image, see `backend/Dockerfile` for better
      control (like running webpack dev server in addition to normal API endpoints server)

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-mkdir -p ./node_modules
+mkdir -p node_modules
 ln -sf /save/node_modules/* ./node_modules/.
 ln -sf /save/node_modules/.bin ./node_modules/.bin
 ln -sf /save/node_modules/.cache ./node_modules/.cache
-gatsby develop -H 0.0.0.0
+./node_modules/.bin/strapi develop --watch-admin --browser false
